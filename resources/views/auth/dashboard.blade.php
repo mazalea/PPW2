5e404633fc69707d3e92dbcc66f74800882e161a
mazalea <azalea.nareswari@gmail.com>
2023-10-24 17:27:55 +0700

    first commit

---
+++ resources/views/auth/dashboard.blade.php
@@ -0,0 +1,180 @@
+@extends('auth.layouts')
+
+@section('content')
+
+    <section id="home" class="home">
+        <div class="container">
+            <div class="text-home align-items-center">
+                <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="2s">
+                    <div class="head_title">
+                        <h2>Hi! I'm Maria Azalea: A Young UI/UX Designer</h2>
+                        <div class="separator"></div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInRight"  data-wow-duration="2s">
+                    <p>Fingerstache green juice semiotics quinoa, truffaut letterpress wolf photo booth ugh biodiesel mumblecore brunch. Meh austin vinyl banh mi, gluten-free chillwave scenester kombucha street art. Before they sold out lumbersexual chillwave, cold-pressed mumblecore beard pickled letterpress normcore crucifix.</p>
+                </div>
+            </div>
+        </div>
+    </section>
+
+    <section id="about" class="about">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="2s">
+                    <div class="head_title text-center">
+                        <h2>About Me</h2>
+                        <div class="separator"></div>
+                    </div>
+                </div>
+                <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInRight"  data-wow-duration="2s">
+                    <div class="head_title_rigth">
+                        <p>Fingerstache green juice semiotics quinoa, truffaut letterpress wolf photo booth ugh biodiesel mumblecore brunch. Meh austin vinyl banh mi, gluten-free chillwave scenester kombucha street art. Before they sold out lumbersexual chillwave, cold-pressed mumblecore beard pickled letterpress normcore crucifix.</p>
+                    </div>
+                </div>
+                <div class="row main_about_content text-center">
+                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="1.5s">
+                        <div class="single_about">
+                            <div class="single_about_img">
+                                <img src="assets/images/skil1.png" alt="" />
+                            </div>
+                            <h3><span class="statistic-counter">30.25</span>Curabitur</h3>
+                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="2s">
+                        <div class="single_about">
+                            <div class="single_about_img">
+                                <img src="assets/images/skil1.png" alt="" />
+                            </div>
+                            <h3><span class="statistic-counter">20.75</span>spanCurabitur</h3>
+                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="3s">
+                        <div class="single_about">
+                            <div class="single_about_img">
+                                <img src="assets/images/skil1.png" alt="" />
+                            </div>
+                            <h3><span class="statistic-counter">15.20</span>Curabitur</h3>
+                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                        </div>
+                    </div> 
+                </div>
+            </div>
+        </div>
+    </section><!-- End of About Section -->
+
+    <section id="whatido" class="whatido">
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="head_title text-center wow fadeInLeft"  data-wow-duration="3s">
+                        <h2>My Projects</h2>
+                        <div class="separator"></div>
+                    </div>
+                </div>
+                
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="head_title_rigth wow fadeInRight"  data-wow-duration="3s">
+                        <p>Fingerstache green juice semiotics quinoa, truffaut letterpress wolf photo booth ugh biodiesel mumblecore brunch. Meh austin vinyl banh mi, gluten-free chillwave scenester kombucha street art. Before they sold out lumbersexual chillwave, cold-pressed mumblecore beard pickled letterpress normcore crucifix.</p>
+                    </div>
+                </div><!-- End of col-sm-5 -->
+                <div class="row main_whatido_content text-center">
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="single_whatido wow fadeInDown"  data-wow-duration="1s">
+                            <div class="single_whatido_img">
+                                <img src="../images/exp/exp_bootstrap.svg" alt="" />
+                            </div>
+                            <h3>ultricies</h3>
+                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                            <div class="btn_bg wow fadeInLeft"  data-wow-duration="3s">
+                            <a href="" class="btn">Load More</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="single_whatido wow fadeInDown"  data-wow-duration="3s">
+                            <div class="single_whatido_img">
+                                <img src="assets/images/what2.png" alt="" />
+                            </div>
+                            <h3>porttitor</h3>
+                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                            <div class="btn_bg wow fadeInLeft"  data-wow-duration="3s">
+                            <a href="" class="btn">Load More</a>
+                            </div>
+                        </div>
+                    </div>
+                    
+                    <div class="col-md-4 col-sm-6 col-xs-12">
+                        <div class="single_whatido wow fadeInDown"  data-wow-duration="4s">
+                            <div class="single_whatido_img">
+                                <img src="assets/images/what3.png" alt="" />
+                            </div>
+                            <h3>Curabitur</h3>
+                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                            <div class="btn_bg wow fadeInLeft"  data-wow-duration="3s">
+                            <a href="" class="btn">Load More</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </section><!-- End of Projects Section -->
+
+    <section id="myworks" class="myworks">
+        <div class="container">
+            <div class="row">	
+                <div class="col-md-1 col-md-offset-5 col-sm-1 col-sm-offset-5 col-xs-12 no-padding">
+                    <div class="scrolldownall text-right">
+                        <a href="#about" class="scroll_btnall"></a>
+                    </div>
+                </div><!-- End of col-sm-2 -->
+                <div class="col-md-6 col-sm-6 col-xs-12">
+                    <div class="head_title text-center wow fadeInLeft"  data-wow-duration="2s">
+                        <h2>My Skills</h2>
+                        <div class="separator"></div>
+                    </div>
+                </div>
+                
+            </div>
+        </div>
+        
+        <div class="row main_myworks_content text-center">
+            <div class="single_work_colum col-md-3 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="1.5s">
+                <div class="single_work">
+                    <img src="../images/exp/exp_bootstrap.svg" alt="" />
+                </div>
+            </div>
+            <div class="single_work_colum col-md-3 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="2s">
+                <div class="single_work">
+                    <img src="../images/exp/exp_css3.svg" alt="" />
+                </div>
+            </div>
+            <div class="single_work_colum col-md-3 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="3s">
+                <div class="single_work">
+                    <img src="../images/exp/exp_html5.svg" alt="" />
+                </div>
+            </div>
+            <div class="single_work_colum col-md-3 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="4s">
+                <div class="single_work">
+                    <img src="assets/images/work4.jpg" alt="" />
+                </div>
+            </div>
+            
+            <div class="btn_bg wow fadeInLeft"  data-wow-duration="3s">
+            <a href="" class="btn">Load More</a>
+            </div>
+        </div>
+    </section><!-- End of Skills Section -->
+
+    <section id="contact" class="py-5">
+        <h2 class="text-center">Contact</h2>
+        <p class="text-center">Contact information: your.email@example.com</p>
+    </section>
+
+@endsection
---
+++ resources/views/auth/layouts.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Laravel 10 Custom User Registration & Login Tutorial - AllPHPTricks.com</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+  </head>
+<body>
+<nav class="navbar navbar-expand-lg" id="navbar-menu">
+  <div class="container">
+    <a class="navbar-brand" href="{{URL('/')}}"><span class="h3 fw-bold">Maria Azalea</span></a>
+    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+      <span class="navbar-toggler-icon"></span>
+    </button>
+    <div class="collapse navbar-collapse" id="navbarNavDropdown">
+      <ul class="navbar-nav ms-auto">
+        @guest
+        <li class="nav-item">
+          <a class="nav-link {{(request()->is('login'))?'active':''}}" href="{{route('login')}}">Login</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link {{(request()->is('register'))?'active':''}}" href="{{route('register')}}">Register</a>
+        </li>
+        @else
+        <li class="nav-item">
+          <a class="nav-link" href="#home">Home</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#about">About</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#whatido">Projects</a>
+        </li>
+        <li class="nav-item">
+          <a class="nav-link" href="#myworks">Skills</a>
+        </li>
+        <li class="nav-item dropdown">
+          <a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+            {{Auth::user()->name}}
+          </a>
+          <ul class="dropdown-menu">
+            <li><a class="dropdown-item" href="{{route('logout')}}" onclick="event.preventDefault();
+            document.getElementById('logout-form').submit();">Logout</a>
+                <form id="logout-form" action="{{route('logout')}}" method="POST">
+                    @csrf 
+                </form>
+            </li>
+          </ul>
+        </li>
+        @endguest
+      </ul>
+    </div>
+  </div>
+</nav>
+
+<div class="content">
+    @yield('content')
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-C6RzsynM9kWDrMNeT87bh95OGNyZPhcTNXj1NW7RuBCsyN/o0jlpcV8Qyq46cDfL" crossorigin="anonymous"></script>
+</body>
+</html>
