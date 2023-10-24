6deb5c6f446a749874617163d894eb9bcf1d7049
mazalea <azalea.nareswari@gmail.com>
2023-10-24 18:38:59 +0700

    second

--- resources/views/auth/dashboard.blade.php
+++ resources/views/auth/dashboard.blade.php
@@ -12,60 +12,11 @@
                     </div>
                 </div>
                 <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInRight"  data-wow-duration="2s">
-                    <p>Fingerstache green juice semiotics quinoa, truffaut letterpress wolf photo booth ugh biodiesel mumblecore brunch. Meh austin vinyl banh mi, gluten-free chillwave scenester kombucha street art. Before they sold out lumbersexual chillwave, cold-pressed mumblecore beard pickled letterpress normcore crucifix.</p>
+                    <p id="p-home">A second year Software Engineering Technology student with a deep passion for UI/UX design. Acquired basic knowledge in software design and web development with coursework and practical projects during my time in college. Familiar with Figma and highly motivated to continually expand my skill set and grow as a designer.</p>
                 </div>
             </div>
         </div>
     </section>
-
-    <section id="about" class="about">
-        <div class="container">
-            <div class="row">
-                <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="2s">
-                    <div class="head_title text-center">
-                        <h2>About Me</h2>
-                        <div class="separator"></div>
-                    </div>
-                </div>
-                <div class="col-md-6 col-sm-6 col-xs-12 wow fadeInRight"  data-wow-duration="2s">
-                    <div class="head_title_rigth">
-                        <p>Fingerstache green juice semiotics quinoa, truffaut letterpress wolf photo booth ugh biodiesel mumblecore brunch. Meh austin vinyl banh mi, gluten-free chillwave scenester kombucha street art. Before they sold out lumbersexual chillwave, cold-pressed mumblecore beard pickled letterpress normcore crucifix.</p>
-                    </div>
-                </div>
-                <div class="row main_about_content text-center">
-                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="1.5s">
-                        <div class="single_about">
-                            <div class="single_about_img">
-                                <img src="assets/images/skil1.png" alt="" />
-                            </div>
-                            <h3><span class="statistic-counter">30.25</span>Curabitur</h3>
-                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
-                        </div>
-                    </div>
-                    
-                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="2s">
-                        <div class="single_about">
-                            <div class="single_about_img">
-                                <img src="assets/images/skil1.png" alt="" />
-                            </div>
-                            <h3><span class="statistic-counter">20.75</span>spanCurabitur</h3>
-                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
-                        </div>
-                    </div>
-                    
-                    <div class="col-md-4 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="3s">
-                        <div class="single_about">
-                            <div class="single_about_img">
-                                <img src="assets/images/skil1.png" alt="" />
-                            </div>
-                            <h3><span class="statistic-counter">15.20</span>Curabitur</h3>
-                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
-                        </div>
-                    </div> 
-                </div>
-            </div>
-        </div>
-    </section><!-- End of About Section -->
 
     <section id="whatido" class="whatido">
         <div class="container">
@@ -79,19 +30,19 @@
                 
                 <div class="col-md-6 col-sm-6 col-xs-12">
                     <div class="head_title_rigth wow fadeInRight"  data-wow-duration="3s">
-                        <p>Fingerstache green juice semiotics quinoa, truffaut letterpress wolf photo booth ugh biodiesel mumblecore brunch. Meh austin vinyl banh mi, gluten-free chillwave scenester kombucha street art. Before they sold out lumbersexual chillwave, cold-pressed mumblecore beard pickled letterpress normcore crucifix.</p>
+                        <p>Welcome to my portfolio of projects. I'm passionate about creating innovative and user-friendly digital experiences. Check out some of my recent work below.</p>
                     </div>
                 </div><!-- End of col-sm-5 -->
                 <div class="row main_whatido_content text-center">
                     <div class="col-md-4 col-sm-6 col-xs-12">
                         <div class="single_whatido wow fadeInDown"  data-wow-duration="1s">
                             <div class="single_whatido_img">
-                                <img src="../images/exp/exp_bootstrap.svg" alt="" />
+                                <img src="../images/project-1.png" alt="" />
                             </div>
-                            <h3>ultricies</h3>
-                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                            <h3>Project 1</h3>
+                            <p>This is a description of my first project. It involved creating a responsive website using Bootstrap and ensuring a seamless user experience.</p>
                             <div class="btn_bg wow fadeInLeft"  data-wow-duration="3s">
-                            <a href="" class="btn">Load More</a>
+                            <a href="" class="btn">See More</a>
                             </div>
                         </div>
                     </div>
@@ -99,12 +50,12 @@
                     <div class="col-md-4 col-sm-6 col-xs-12">
                         <div class="single_whatido wow fadeInDown"  data-wow-duration="3s">
                             <div class="single_whatido_img">
-                                <img src="assets/images/what2.png" alt="" />
+                                <img src="../images/project-2.png" alt="" />
                             </div>
-                            <h3>porttitor</h3>
-                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                            <h3>Project 2</h3>
+                            <p>Project 2 involved designing a user-friendly mobile app with a focus on intuitive navigation and a modern look and feel.</p>
                             <div class="btn_bg wow fadeInLeft"  data-wow-duration="3s">
-                            <a href="" class="btn">Load More</a>
+                            <a href="" class="btn">See More</a>
                             </div>
                         </div>
                     </div>
@@ -112,12 +63,12 @@
                     <div class="col-md-4 col-sm-6 col-xs-12">
                         <div class="single_whatido wow fadeInDown"  data-wow-duration="4s">
                             <div class="single_whatido_img">
-                                <img src="assets/images/what3.png" alt="" />
+                                <img src="../images/project-3.png" alt="" />
                             </div>
-                            <h3>Curabitur</h3>
-                            <p>Four dollar toast messenger bag green juice, organic cliche tofu synth small batch locavore austin. Squid mlkshk authe ntic pinterest, pour-over gastropub XOXO seitan sartorial humblebrag. </p>
+                            <h3>Project 3</h3>
+                            <p>Project 3 was about creating an innovative and user-friendly mobile app that provided a seamless experience for users.</p>
                             <div class="btn_bg wow fadeInLeft"  data-wow-duration="3s">
-                            <a href="" class="btn">Load More</a>
+                            <a href="" class="btn">See More</a>
                             </div>
                         </div>
                     </div>
@@ -162,7 +113,7 @@
             </div>
             <div class="single_work_colum col-md-3 col-sm-6 col-xs-12 wow fadeInLeft"  data-wow-duration="4s">
                 <div class="single_work">
-                    <img src="assets/images/work4.jpg" alt="" />
+                    <img src="../images/exp/exp_javascript.svg" alt="" />
                 </div>
             </div>
             
@@ -171,10 +122,5 @@
             </div>
         </div>
     </section><!-- End of Skills Section -->
-
-    <section id="contact" class="py-5">
-        <h2 class="text-center">Contact</h2>
-        <p class="text-center">Contact information: your.email@example.com</p>
-    </section>
 
 @endsection
--- resources/views/auth/layouts.blade.php
+++ resources/views/auth/layouts.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Laravel 10 Custom User Registration & Login Tutorial - AllPHPTricks.com</title>
+    <title>Portfolio | Maria Azalea Nareswari</title>
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
     <link rel="stylesheet" href="{{ asset('css/style.css') }}">
   </head>
@@ -27,9 +27,6 @@
         @else
         <li class="nav-item">
           <a class="nav-link" href="#home">Home</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#about">About</a>
         </li>
         <li class="nav-item">
           <a class="nav-link" href="#whatido">Projects</a>
